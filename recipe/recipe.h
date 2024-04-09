d8f3b03884c68ec31defbc174e886287345380de
Paul Santiago <70616143+prsanti@users.noreply.github.com>
2024-04-09 15:19:41 -0400

    complete recipe struct

--- recipe/recipe.h
+++ recipe/recipe.h
@@ -2,15 +2,23 @@
 
 // PROG71985 - Winter 2024 - Group 12s
 #define MAXNAME	50
+#define MAXINGREDIENT	99
+#define MAXINSTRUCTION	500
+#define MAXLIST	15
 
 typedef struct recipe {
 	int recipeNumber;
 	char name[MAXNAME];
-	struct {
-		char ingredients[MAXNAME];
-		int amount;
-	};
-
-	// steps
-
+	INGREDIENTS ingredientList[MAXLIST];
+	STEPS stepsList[MAXLIST];
 } RECIPE;
+
+typedef struct ingredients {
+	float quantity;
+	char ingredient[MAXINGREDIENT];
+} INGREDIENTS;
+
+typedef struct steps {
+	int step;
+	char instruction[MAXINSTRUCTION];
+} STEPS;
